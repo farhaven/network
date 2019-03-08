70205cf270f6abad01344069d401de545151de64
Gregor Best <gbe@unobtanium.de>
2019-03-08 20:37:51 +0100

    Start of MNIST evaluation test
    It's frickin' slow.

--- src/mnist_loader.rs
+++ src/mnist_loader.rs
@@ -8,7 +8,7 @@ pub enum MNistLoadError {
     String(String),
 }
 
-pub fn load_labels(path: &str) -> Result<Vec<u8>, MNistLoadError> {
+pub fn load_labels(path: &str) -> Result<Vec<Vec<f64>>, MNistLoadError> {
     let mut file = match File::open(path) {
         Ok(f) => f,
         Err(x) => return Err(MNistLoadError::IOError(x))
@@ -29,13 +29,24 @@ pub fn load_labels(path: &str) -> Result<Vec<u8>, MNistLoadError> {
         Err(x) => return Err(MNistLoadError::IOError(x))
     };
 
-    let mut res: Vec<u8> = vec![];
+    let mut res: Vec<Vec<f64>> = vec![];
     loop {
         let val = match file.read_u8() {
             Ok(v) => v,
             Err(_) => break
         };
-        res.push(val);
+
+        if val > 10 {
+            return Err(MNistLoadError::String(format!("Unexpected label {}", val)));
+        }
+
+        let mut onehot: Vec<f64> = Vec::with_capacity(10);
+
+        for idx in 0..10 {
+            onehot.push(if idx == val { 1_f64 } else { 0_f64 });
+        }
+
+        res.push(onehot);
     }
 
     if res.len() != numitems as usize {
@@ -45,7 +56,7 @@ pub fn load_labels(path: &str) -> Result<Vec<u8>, MNistLoadError> {
     Ok(res)
 }
 
-pub fn load_images(path: &str) -> Result<Vec<Vec<u8>>, MNistLoadError> {
+pub fn load_images(path: &str) -> Result<Vec<Vec<f64>>, MNistLoadError> {
     let mut file = match File::open(path) {
         Ok(f) => f,
         Err(x) => return Err(MNistLoadError::IOError(x))
@@ -76,7 +87,7 @@ pub fn load_images(path: &str) -> Result<Vec<Vec<u8>>, MNistLoadError> {
         Err(x) => return Err(MNistLoadError::IOError(x))
     };
 
-    let mut res: Vec<Vec<u8>> = vec![];
+    let mut res: Vec<Vec<f64>> = vec![];
     for _i in 0..numitems {
         let numbytes = numcols as usize * numrows as usize;
         let mut img: Vec<u8> = Vec::with_capacity(numbytes);
@@ -93,7 +104,10 @@ pub fn load_images(path: &str) -> Result<Vec<Vec<u8>>, MNistLoadError> {
         if sz != numbytes {
             return Err(MNistLoadError::String(format!("Read {} bytes, expected {}", sz, numbytes)))
         }
-        res.push(img);
+
+        let val: Vec<f64> = img.iter().map(|&x| x as f64).collect();
+
+        res.push(val);
     }
 
     if res.len() != numitems as usize {
