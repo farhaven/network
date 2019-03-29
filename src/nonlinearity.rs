78537fa5cee5d3baa925934dafe5a6c2d1ef4290
Gregor Best <gbe@unobtanium.de>
2019-03-29 16:16:23 +0100

    Fix sigmoid

--- src/nonlinearity.rs
+++ src/nonlinearity.rs
@@ -9,7 +9,7 @@ impl Nonlinearity {
     pub fn forward(&self, z: &f64) -> f64 {
         match self {
             Nonlinearity::Tanh => z.tanh(),
-            Nonlinearity::Sigmoid => (1.0 + z.exp()).ln(),
+            Nonlinearity::Sigmoid => 1.0 / (1.0 + (-z).exp()),
             Nonlinearity::LeakyELU => if z >= &0.0 {
                 *z
             } else {
@@ -22,7 +22,7 @@ impl Nonlinearity {
     pub fn backward(&self, z: &f64) -> f64 {
         match self {
             Nonlinearity::Tanh => 1_f64 - z.powf(2_f64),
-            Nonlinearity::Sigmoid => 1.0 / (1.0 + (-z).exp()),
+            Nonlinearity::Sigmoid => self.forward(z) * (1.0 - self.forward(z)),
             Nonlinearity::LeakyELU => if z > &0.0 {
                     1.0
                 } else if z == &0.0 {
